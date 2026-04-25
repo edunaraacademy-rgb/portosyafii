0c8b9ceb0e9fbc9eab39437bb6a2c8807d74868a
Ridho Robbi Pasi <ridhorobbipasi@gmail.com>
2026-04-25 20:22:07 +0700

    Config: whitelist Supabase storage domain in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,9 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        hostname: 'fdaecqrdcgnfacikpcza.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
       },
     ],
   },
